26a9477d8422b5771a04a1658eaeecad0cc8f7b0
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-15 06:05:43 -0600

    final

---
+++ index.js
@@ -0,0 +1,33 @@
+const languageSelector = document.getElementById("language-selector")
+const greetingDisplay = document.getElementById("greeting-text")
+languageSelector.addEventListener("change", translate)
+
+const greetingsArr = [
+    {
+        language: "English",
+        greeting: "Merry Christmas!"
+    },
+    {
+        language: "Spanish",
+        greeting: "Feliz Navidad!"
+    },
+    {
+        language: "Ukrainian",
+        greeting: "щасливого Різдва!"
+    },
+    {
+        language: "Welsh",
+        greeting: "Nadolig Llawen!"
+    }
+]
+
+function translate(){
+    // Task: 
+    // - Write a function to display the correct greeting when a language is selected.
+    const display = greetingsArr.find(greet => greet.language === languageSelector.value)
+    greetingDisplay.textContent = display.greeting
+}
+
+// Stretch goals:
+// - Animate the message switch.
+// - Add the option to switch the greeting, for example also have "And a Happy New Year!".
